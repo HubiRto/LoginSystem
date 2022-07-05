0c24a9da3144aa6554f1afecfe8914de552e2177
HubiRto <hubert.rybicki.hr@gmail.com>
2022-07-05 21:55:01 +0200

    Initial commit

---
+++ src/main/java/pl/pomoku/loginsystem/LoginSystem.java
@@ -0,0 +1,17 @@
+package pl.pomoku.loginsystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LoginSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
