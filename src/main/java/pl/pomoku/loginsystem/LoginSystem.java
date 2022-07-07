31b85abcc2d515ecde064e004d596a9e2b86514e
HubiRto <hubert.rybicki.hr@gmail.com>
2022-07-07 13:39:48 +0200

    Podlaczenie do bazy danych.

--- src/main/java/pl/pomoku/loginsystem/LoginSystem.java
+++ src/main/java/pl/pomoku/loginsystem/LoginSystem.java
@@ -2,12 +2,40 @@ package pl.pomoku.loginsystem;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public final class LoginSystem extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        String url = "jdbc:mysql://localhost/users";
+        String user = "root";
+        String password = "";
+
+        Connection con = null;
+
+        try{
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the Users Database.");
+        }catch (SQLException e){
+            System.out.println("Unable to connect to the Users Database.");
+            e.printStackTrace();
+        }
+        try{
+            Statement statement = con.createStatement();
+            String sql = "CREATE TABLE IF NOT EXIST players(uuid int)";
+            statement.execute(sql);
+            statement.close();
+            con.close();
+            System.out.println("Create the players table in the database.");
+        }catch (SQLException e){
+            System.out.println("Unable to create the players table in the database.");
+            e.printStackTrace();
+        }
     }
 
     @Override
